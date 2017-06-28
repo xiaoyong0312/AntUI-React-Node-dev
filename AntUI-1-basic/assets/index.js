a74d87c26041047664c27413de57119b433b184f
xuyong <xuyong@qianmi.com>
2017-06-28 17:58:03 +0800

    1-basic-代码整理，

---
+++ AntUI-1-basic/assets/index.js
@@ -0,0 +1,45 @@
+/**
+ * @desc AntUIApp is my first AntUI example;
+ * @author Create by xuyong on 2017/6/18
+ */
+
+import React, {Component} from 'react';
+import ReactDom, {render} from 'react-dom';
+//import {StoreProvider} from 'iflux2';
+//import AppStore from './store';
+
+import { Button, Toast } from 'antd-mobile';
+//import Button from 'antd-mobile/lib/button';
+
+//debug: true, it will show good logs
+//@StoreProvider(AppStore, {debug: true})
+export default class AntUIApp extends React.Component {
+
+    componentDidMount() {
+
+    }
+
+    render() {
+
+        return (
+            <div>
+                <a href="https://www.baidu.com" target="_blank">
+                    This is AntUIApp.
+                </a>
+
+                <hr/>
+
+                <Button onClick={()=>{
+                    console.log(`AntUI click...`);
+                    //Toast.info('AntUI click', 1);
+                    //Toast.info('This is a toast tips !!!', 1);
+                    //alert('AntUI click');
+                }}>AntUI</Button>
+            </div>
+        );
+    }
+
+}
+
+render(<AntUIApp/>, document.getElementById('antUI'));
+//ReactDom.render(<AntUIApp/>, document.getElementById('antUI'));
